f5f6859b32bb1277474d73cab99616e05570204e
vsm2004 <128446657+vsm2004@users.noreply.github.com>
2024-07-11 00:56:07 +0530

    Add files via upload

---
+++ sales prediction.py
@@ -0,0 +1,13 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+p = pd.read_csv("C:\\Users\\DELL\\Desktop\\python projects and achievements\\advertising.csv")
+model = LinearRegression()
+X = p.drop('Sales', axis='columns')
+y = p['Sales']
+model.fit(X,y)
+model.score(X,y)
+print(model.predict([[44,21,19]]))
+print(model.coef_)
+print(model.intercept_)
